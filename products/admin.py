c5a88b4e2c5c7cbd125da722d8619d034ce6a85f
Sabaresh <sabaresh1205@gmail.com>
2026-05-05 14:43:01 +0530

    fix admin duplicate error

--- products/admin.py
+++ products/admin.py
@@ -1,13 +1,22 @@
 from django.contrib import admin
 from .models import Products, Partners, Customers, Orders, OrderItems
+from django.utils.html import format_html
 
-admin.site.register(Products)
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('product_name', 'product_price', 'image_preview')
+
+    def image_preview(self, obj):
+        if obj.product_cover_image:
+            return format_html('<img src="{}" width="100" />', obj.product_cover_image)
+        return "No Image"
+
+    image_preview.short_description = "Image"
+
+
+# ✅ REGISTER ONLY ONCE
+admin.site.register(Products, ProductAdmin)
 admin.site.register(Partners)
 admin.site.register(Customers)
 admin.site.register(Orders)
 admin.site.register(OrderItems)
-
-class ProductAdmin(admin.ModelAdmin):
-    list_display = ('product_name', 'product_price', 'product_cover_image')
-
-admin.site.register(Products, ProductAdmin)
